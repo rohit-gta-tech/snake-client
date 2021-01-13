d55ea35b8bee91a9e63c6297c2d657134fb8d48f
Rohit <rohitcricbuzz@gmail.com>
2021-01-13 18:14:26 +0000

    Tested how to move snke in client js

--- client.js
+++ client.js
@@ -11,9 +11,24 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect' , () => {
+      //displays message on my client when connection is established
       console.log("Successfully connected to game server");
+      //displays my name on the server playboard as soon as I am am connected
       conn.write("Name: RST");
+      //instructs the server to move my snake up
+      /*conn.write("Move: up");
+      for (let i = 0; i < 100; i ++) {
+        setTimeout(() => {
+          conn.write("Move: up")
+        }, i*1000)
+        setTimeout(() => {
+          conn.write("Move: right")
+        }, i*1000+500)
+      }*/
   })
+
+
+
   conn.on('data', (data) => {
       console.log(data);
   })
