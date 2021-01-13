2b02431779064bb20d82151db5823de24d2e2422
Rohit <rohitcricbuzz@gmail.com>
2021-01-13 19:00:35 +0000

    play file refactored to export input from input module

---
+++ input.js
@@ -0,0 +1,18 @@
+//function for handling user input
+const handleUserInput = function(key) {
+    if (key === '\u0003') {
+        process.exit();
+    }
+}
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    //stdin will take in handleUserInput as its callback function for handling the keyboard inputs
+    stdin.on('data', handleUserInput)
+    return stdin;
+}
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-//function for handling user input
-const handleUserInput = function(key) {
-    if (key === '\u0003') {
-        process.exit();
-    }
-}
+setupInput();
 
-const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    //stdin will take in handleUserInput as its callback function for handling the keyboard inputs
-    stdin.on('data', handleUserInput)
-    return stdin;
-  }
-
-  setupInput();
-
-  
